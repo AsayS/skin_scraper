1daf0cdd8049f71f31921a0d416eeb073ec05371
AsayS <gvido.ko@gmail.com>
2024-01-10 01:03:44 +0200

    init 2

--- Gp/login.py
+++ Gp/login.py
@@ -1,8 +1,12 @@
 from selenium.webdriver.common.by import By
 import time
+import configparser
 
-
-
+config_object = configparser.ConfigParser()
+with open("../login.ini","r") as file_object:
+    config_object.read_file(file_object)
+    password=config_object.get("login","password")
+    username=config_object.get("login","username")
 
 def logins(driver):
     driver.get('https://www.steamcommunity.com/')
@@ -10,12 +14,12 @@ def logins(driver):
     element.click()
     time.sleep(2.5)
     element = driver.find_element(By.XPATH, '//*[@id="responsive_page_template_content"]/div[1]/div[1]/div/div/div/div[2]/div/form/div[1]/input')
-    element.send_keys("kakamakaka336")
+    element.send_keys(str(username))
 
     time.sleep(3)
 
     element = driver.find_element(By.XPATH, '//*[@id="responsive_page_template_content"]/div[1]/div[1]/div/div/div/div[2]/div/form/div[2]/input')
-    element.send_keys("Edmanasbikses0")
+    element.send_keys(str(password))
 
     time.sleep(2)
 
